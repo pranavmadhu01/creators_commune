fd8dcc9de8c75ddb9c2640fc1c92eb9b3bcbb199
Amal C P <acp070146@gmail.com>
2022-06-29 12:59:23 +0530

    navbar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
-import Navbar from "./components/Navbar/Navbar"
+import Navbar from "./components/Navbar/Navbar";
+import News from "./components/News/News";
 
 function App() {
-  return <div className="App">
-    <Navbar />
-    <Home />
-    <About />
-  </div>;
+    return <div className = "App" >
+        <
+        Navbar / >
+        <
+        Home / >
+        <
+        About / >
+        <
+        News / >
+        <
+        /div>;
 }
 
 export default App;
