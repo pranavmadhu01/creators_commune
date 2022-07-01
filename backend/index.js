4f4b8b204eae1de944f1978ec75ce367fc7ec7aa
pranav madhu <pranavmadhu73@gmail.com>
2022-07-02 00:17:37 +0530

    removed backend folder

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const port = 3030;
-
-const app = express();
-
-app.listen(port, () => {
-  console.log(`iam all set in port ${port}`);
-});
