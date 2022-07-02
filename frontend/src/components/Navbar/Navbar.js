665c725365cd062197dd39f27ab11531b0ebeafa
pranav madhu <pranavmadhu73@gmail.com>
2022-07-02 09:34:26 +0530

    # debugging

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,8 +7,11 @@ import { FaHamburger } from "react-icons/fa";
 
 export default function () {
   const [hamburger, setHamburger] = useState(false);
-  function activehamburger() {
+  function activehamburger(e) {
     setHamburger(!hamburger);
+  }
+  function deactivatehamburger(e){
+    setHamburger(!hamburger)
   }
   return (
     <header className="header">
@@ -16,20 +19,20 @@ export default function () {
       <div className="logo-wrapper">
         <img src={logo} alt="logo of creators commune" />
       </div>
-      <nav className={"navbar" + (hamburger ? "  navbar-active" : "")}>
+      <nav className={"navbar" + (hamburger ? "  navbar-active" : "")} onClick={deactivatehamburger}>
         <ul className="navbar-links-list-wrapper">
           <li className="links-list">
-            <a href="#about" className="navbar-links">
+            <a href="#about" className="navbar-links" onClick={deactivatehamburger}>
               About us
             </a>
           </li>
           <li className="links-list">
-            <a href="#events" className="navbar-links">
+            <a href="#events" className="navbar-links" onClick={deactivatehamburger}>
               Events
             </a>
           </li>
           <li className="links-list">
-            <a href="#team" className="navbar-links">
+            <a href="#team" className="navbar-links" onClick={deactivatehamburger}>
               Our team
             </a>
           </li>
