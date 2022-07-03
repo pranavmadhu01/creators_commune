14514bb2dbc3d5adfb803d3c9833a8607af2f500
pranav madhu <pranavmadhu73@gmail.com>
2022-07-03 08:34:38 +0530

    added particles to background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
+import React, { useMemo, useCallback } from "react";
 
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -10,6 +13,62 @@ import Footer from "./components/Footer/Footer";
 import Totop from "./components/totop/Totop";
 
 function App() {
+  const options = useMemo(() => {
+    return {
+      background: {
+        color: "",
+      },
+      fpsLimit: 120,
+      responsive: [],
+      fullScreen: {
+        enable: true,
+        zIndex: -1,
+      },
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
+          },
+          onHover: {
+            // enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10,
+          },
+          repulse: {
+            distance: 200,
+          },
+        },
+      },
+      particles: {
+        shape: {
+          type: "square",
+        },
+        links: {
+          enable: true,
+          distance: 50,
+        },
+        move: {
+          enable: true,
+          speed: { min: 1, max: 2 },
+        },
+        opacity: {
+          value: { min: 0.3, max: 0.5 },
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+    };
+  }, []);
+
+  const particlesInit = useCallback((engine) => {
+    loadSlim(engine);
+  }, []);
   return (
     <div className="App">
       <Navbar />
@@ -19,6 +78,7 @@ function App() {
       <News />
       <Team />
       <Footer />
+      <Particles init={particlesInit} options={options} />
     </div>
   );
 }
